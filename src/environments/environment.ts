e42d647c21ef64d2d06c42e21761de067e7f815f
Theresa <ymy.theresa@gmail.com>
2019-10-26 19:41:40 +0800

    changed credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,14 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "YOUR_CREDENTIALS_HERE",
-    authDomain: "YOUR_CREDENTIALS_HERE",
-    databaseURL: "YOUR_CREDENTIALS_HERE",
-    projectId: "YOUR_CREDENTIALS_HERE",
-    storageBucket: "YOUR_CREDENTIALS_HERE",
-    messagingSenderId: "YOUR_CREDENTIALS_HERE"
+    apiKey: "AIzaSyAN37selfGurR79KVWbC9YmHgpYZFpOM2A",
+	authDomain: "ionic-todo-app-35fa5.firebaseapp.com",
+	databaseURL: "https://ionic-todo-app-35fa5.firebaseio.com",
+	projectId: "ionic-todo-app-35fa5",
+	storageBucket: "ionic-todo-app-35fa5.appspot.com",
+	messagingSenderId: "517043224440",
+	appId: "1:517043224440:web:c8a928d26af1f370b7128d",
+	measurementId: "G-PG80JTQSVL"
   }
 };
 
